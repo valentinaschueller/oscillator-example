9f6c6cf2b761c08d0c32ffb38d254567525fbc20
Valentina Schueller <valentina.schueller@gmail.com>
2021-11-14 11:56:37 +0100

    Add ERK to Simulation

--- src/equal_stiffness.py
+++ src/equal_stiffness.py
@@ -5,14 +5,14 @@ from numpy.core import numeric
 from plotting import prepare_plot, plot_error_ref
 
 from monolithic_system import MonolithicSystem
-from timestepping import GeneralizedAlpha, NewmarkBeta
+from timestepping import ERK, GeneralizedAlpha, NewmarkBeta
 
 # k1 = k2 = m1 = m2 = 1
 
 def run_simulation(t_stop: int, N: float, solver_str: str = "newmark"):
     ode_system = MonolithicSystem()
 
-    alpha_m = 0.5
+    alpha_m = 0.3
     alpha_f = 0.5
     gamma = 0.5 - alpha_m + alpha_f
     beta = 0.25 * (gamma + 0.5)**2
@@ -20,11 +20,13 @@ def run_simulation(t_stop: int, N: float, solver_str: str = "newmark"):
     newmark_beta = 0.25
     if solver_str == "newmark":
         solver = NewmarkBeta(ode_system.A, newmark_beta, newmark_gamma)
-    else:
+    elif solver_str == "alpha":
         solver = GeneralizedAlpha(
             ode_system.A, ode_system.M, ode_system.K, 
             beta, gamma, alpha_f, alpha_m, 
             ode_system.force)
+    else:
+        solver = ERK(ode_system.first_order_A, 4)
 
     #t = np.linspace(0., t_stop, N+1)
     
@@ -76,11 +78,15 @@ def max_norm(vec):
     return np.max(np.abs(vec))
 
 def compute_newmark_error(t_stop, N):
-    true_sol, num_sol = run_simulation(t_stop, N)
+    true_sol, num_sol = run_simulation(t_stop, N, solver_str="newmark")
     return true_sol - num_sol[0:4,:]
 
 def compute_alpha_error(t_stop, N):
     true_sol, num_sol = run_simulation(t_stop, N, solver_str="alpha")
+    return true_sol - num_sol[0:4,:]
+
+def compute_erk_error(t_stop, N):
+    true_sol, num_sol = run_simulation(t_stop, N, solver_str="erk")
     return true_sol - num_sol[0:4,:]
 
 def create_solution_plots(t, sol, dir_name: str ="plots"):
@@ -107,14 +113,16 @@ if __name__ == '__main__':
     dt_list = np.array([t_stop / N for N in N_list])
     l2_errors_newmark = np.array([l1_norm(compute_newmark_error(t_stop, N)) for N in N_list])
     l2_errors_alpha = np.array([l1_norm(compute_alpha_error(t_stop, N)) for N in N_list])
+    l2_errors_erk = np.array([l1_norm(compute_erk_error(t_stop, N)) for N in N_list])
 
     title = "Errors"
-    subtitle = "k1 = k2 = m1 = m2 = 1"
+    subtitle = r"$\alpha_m = 0.3, \alpha_f = 0.5$"
     xlabel = "dt"
-    ylabel = "error"
+    ylabel = r"$\vert\vert e \vert\vert_2$"
     fig, ax = prepare_plot(title, subtitle, xlabel, ylabel)
     plot_error_ref(ax, dt_list)
     ax.plot(dt_list, l2_errors_newmark, "o-", label="Newmark errors")
     ax.plot(dt_list, l2_errors_alpha, "x-", label="alpha errors")
+    ax.plot(dt_list, l2_errors_erk, "x-", label="erk errors")
     ax.legend()
-    plt.show()
+    plt.savefig("convergence_plot_all.png", dpi=300)
