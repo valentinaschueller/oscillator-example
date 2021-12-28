5ebbc6eba69f9029c865cb18e559b26ae4e5b358
Valentina Schueller <valentina.schueller@gmail.com>
2021-12-28 12:31:04 +0100

    Add Clean Script to Compute & Plot Analytical Solution

--- src/sympy_analytic_solution.py
+++ src/sympy_analytic_solution.py
@@ -1,4 +1,16 @@
 from sympy import *
+from sympy.plotting import plot as symplot
+from sympy import lambdify
+import numpy as np
+from numpy import cos, sin
+import matplotlib.pyplot as plt
+from utility import prepare_plot
+
+k1 = 20
+k2 = 0.1
+k12 = 0.5
+m1 = 1
+m2 = 1
 
 def solve_two_mass_spring_system():
     # two mass-spring-system
@@ -8,15 +20,15 @@ def solve_two_mass_spring_system():
 
     initial_conditions = {
         u1(0): 1,
-        u2(0): 0,
+        u2(0): 0.5,
         v1(0): 0,
         v2(0): 0,
     }
 
     eq_u1 = Eq(u1(t).diff(t), v1(t))
     eq_u2 = Eq(u2(t).diff(t), v2(t))
-    eq_v1 = Eq(v1(t).diff(t), -2 * u1(t) + u2(t))
-    eq_v2 = Eq(v2(t).diff(t), -2 * u2(t) + u1(t))
+    eq_v1 = Eq(v1(t).diff(t), -(k1 + k12)/m1 * u1(t) + k12/m1 * u2(t))
+    eq_v2 = Eq(v2(t).diff(t), -(k2 + k12)/m2 * u2(t) + k12/m2 * u1(t))
 
     result = dsolve(
         [eq_u1, eq_u2, eq_v1, eq_v2],
@@ -24,6 +36,11 @@ def solve_two_mass_spring_system():
         ics=initial_conditions,
         )
     print(result)
+    p1 = symplot(result[0].rhs, (t,0,20), line_color="tab:blue", show=False)
+    p2 = symplot(result[1].rhs, (t,0,20), line_color="tab:orange", show=False)
+    p1.extend(p2)
+    p1.show()
+
 
 if __name__ == '__main__':
     solve_two_mass_spring_system()
