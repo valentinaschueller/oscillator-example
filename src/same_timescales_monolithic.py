7d70301b50cad02721d6150f238fa05ef465f57e
Valentina Schueller <valentina.schueller@gmail.com>
2022-01-02 20:04:32 +0100

    Update with Semi-Final Plotting

--- src/same_timescales_monolithic.py
+++ src/same_timescales_monolithic.py
@@ -26,10 +26,12 @@ def run_simulation(t_stop: int, N: float, solver_str: str = "newmark"):
             ode_system.A_second_order, ode_system.M, ode_system.K, 
             beta, gamma, alpha_f, alpha_m, 
             ode_system.second_order_force)
-    else:
+    elif solver_str == "erk4":
         solver = ERK(ode_system.A_first_order, force_function=ode_system.first_order_force , order=4)
-
-    #t = np.linspace(0., t_stop, N+1)
+    elif solver_str == "erk1":
+        solver = ERK(ode_system.A_first_order, force_function=ode_system.first_order_force , order=1)
+    else:
+        raise NotImplementedError(f"Solver {solver_str} not implemented!")
     
     analytical_solution = ode_system.analytical_solution(t_stop, N)
 
@@ -45,9 +47,15 @@ def compute_alpha_error(t_stop, N):
     true_sol, num_sol = run_simulation(t_stop, N, solver_str="alpha")
     return true_sol - num_sol[0:4,:]
 
-def compute_erk_error(t_stop, N):
-    true_sol, num_sol = run_simulation(t_stop, N, solver_str="erk")
+def compute_erk_error(t_stop, N, order:int):
+    true_sol, num_sol = run_simulation(t_stop, N, solver_str=f"erk{order}")
     return true_sol - num_sol[0:4,:]
+
+def beautify_plot(ax):
+    # remove top and right spine
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    return ax
 
 if __name__ == '__main__':
     # N = 100
@@ -58,20 +66,22 @@ if __name__ == '__main__':
     # create_solution_plots(t, true_sol)
     
     t_stop = 20
-    N_list = np.array([500, 1000, 2000, 4000, 8000])
+    N_list = np.array([125, 250, 500, 1000, 2000, 4000, 8000])
     dt_list = np.array([t_stop / N for N in N_list])
     max_errors_newmark = np.array([max_norm(compute_newmark_error(t_stop, N)) for N in N_list])
     max_errors_alpha = np.array([max_norm(compute_alpha_error(t_stop, N)) for N in N_list])
-    max_errors_erk = np.array([max_norm(compute_erk_error(t_stop, N)) for N in N_list])
+    max_errors_erk4 = np.array([max_norm(compute_erk_error(t_stop, N, 4)) for N in N_list])
+    max_errors_erk1 = np.array([max_norm(compute_erk_error(t_stop, N, 1)) for N in N_list])
 
-    title = "Monolithic System: Convergence Plot"
-    subtitle = r"$\alpha_m = 0.2, \alpha_f = 0.5$"
-    xlabel = "dt"
+    title = "Monolithic System (same time scales): Convergence Study"
+    xlabel = r"$\Delta t$"
     ylabel = r"$\left\| e \right\|_\infty$"
-    fig, ax = prepare_plot(title, subtitle, xlabel, ylabel)
+    fig, ax = prepare_plot(title, "", xlabel, ylabel)
     plot_error_ref(ax, dt_list)
-    ax.plot(dt_list, max_errors_newmark, linestyle="none", marker=".", color="C9", label=r"Newmark $\beta$")
-    ax.plot(dt_list, max_errors_alpha, linestyle="none", marker="x", color="C6", label=r"Generalized $\alpha$")
-    ax.plot(dt_list, max_errors_erk, linestyle="none", marker="1", color="C8", label=r"ERK4")
-    ax.legend()
-    plt.savefig("convergence_plot_all.png", dpi=300, bbox_inches='tight')
+    ax.plot(dt_list, max_errors_erk1, linestyle="none", marker="3", color="maroon", label=r"ERK1")
+    ax.plot(dt_list, max_errors_newmark, linestyle="none", marker=".", color="darkcyan", label=r"Newmark-$\beta$")
+    ax.plot(dt_list, max_errors_alpha, linestyle="none", marker="x", color="darkorchid", label=r"generalized-$\alpha$")
+    ax.plot(dt_list, max_errors_erk4, linestyle="none", marker="1", color="olive", label=r"ERK4")
+    ax.legend(ncol=2, loc='lower right')
+    ax = beautify_plot(ax)
+    plt.savefig("convergence_same_timescales_monolithic.pdf", dpi=300, bbox_inches='tight')
