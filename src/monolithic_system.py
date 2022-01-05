a153b04f1eb42212c7f5e96d8d923ba31617e80f
Valentina Schueller <valentina.schueller@gmail.com>
2022-01-05 16:10:42 +0100

    Add Monolithic Solution for Different Time Scales

--- src/monolithic_system.py
+++ src/monolithic_system.py
@@ -62,3 +62,25 @@ class SameTimescales(MonolithicSystem):
             0.5 * (- np.sin(t) + np.sqrt(3) * np.sin(np.sqrt(3)*t)),
         ])
         return result
+
+class DiffTimescales(MonolithicSystem):
+    def __init__(self):
+        super().__init__(20, 0.1, 0.5, 1, 1)
+    
+    def analytical_solution(self, t_end: float, N: int):
+        t = np.linspace(0, t_end, N+1)
+        w1 = 0.766449676626696
+        w2 = 4.5290788128712
+        result = np.array([
+            0.0131770864007018*np.cos(w1 * t) + 0.986822913599298*np.cos(w2 * t),
+            0.524778912572798*np.cos(w1 * t) - 0.0247789125727997*np.cos(w2 * t),
+            -0.0100995736107*np.sin(w1 * t) - 4.46939875003841*np.sin(w2 * t),
+            -0.402216627841931*np.sin(w1 * t) + 0.112225647939455*np.sin(w2 * t),
+        ])
+        return result
+    
+    def _initial_conditions(self):
+        u0 = np.array([1., .5])
+        v0 = np.array([0., 0.])
+        a0 = np.dot(self.A_second_order, u0)
+        return np.concatenate([u0, v0, a0])
