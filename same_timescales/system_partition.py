7b389f69c22ea92cfdc97446607318040b94bb89
Valentina Schueller <valentina.schueller@gmail.com>
2022-01-06 15:24:47 +0100

    Rework Folder Structure Once More

---
+++ same_timescales/system_partition.py
@@ -0,0 +1,116 @@
+import numpy as np
+from utility import interpolate_linear
+
+class SystemPartition:
+    def __init__(self,
+                left_system: bool,
+                t_end: float = 0.,
+                N: int = 0,
+                result_values: int = 0,
+                k1: int = 1,
+                k2: int = 1,
+                k12: int = 1,
+                m1: int = 1,
+                m2: int = 1):
+        self.left_system_bool = left_system
+        self.k12 = k12
+        self.interpolation_order = 0
+        
+        if left_system:
+            # for the formulation: Mu'' + Ku = 0:
+            self.M = m1 * np.eye(1, 1, dtype=float)
+            self.K = (k1 + k12) * np.eye(1, 1, dtype=float)
+            # for the formulation: y' = A_first_order * y:
+            self.A_first_order = np.array(
+                [[0., 1.],
+                [-(k1 + k12)/m1, 0.]]
+            )
+            # for the formulation: u'' = A_second_order * u:
+            self.A_second_order = -(k1 + k12) * np.eye(1, 1, dtype=float)
+        else:
+            # for the formulation: Mu'' + Ku = 0:
+            self.M = m2 * np.eye(1, 1, dtype=float)
+            self.K = (k2 + k12) * np.eye(1, 1, dtype=float)
+            # for the formulation: y' = A_first_order * y:
+            self.A_first_order = np.array(
+                [[0., 1.],
+                [-(k2 + k12)/m2, 0.]]
+            )
+            # for the formulation: u'' = A_second_order * u:
+            self.A_second_order = -(k2 + k12) * np.eye(1, 1, dtype=float)
+
+        self.result = np.zeros((result_values, N+1))
+        self.t_values = np.linspace(0, t_end, N+1)
+        self.dt = t_end/N
+        self.other_u = np.full(N+1, np.inf)
+        self.other_u[0] = self._initial_other_u()
+        try:
+            self.result[:, 0] = self._initial_conditions()
+        except ValueError:
+            self.result[:, 0] = self._initial_conditions()[:2]
+
+    def other_u_at(self, t, t_lower):
+        idx = np.where(np.abs(self.t_values - t_lower) < 1e-6)[0][0]
+        # print(f"t_lower: {t_lower}, idx: {idx}")
+        if self.interpolation_order == 0:
+            if self.other_u[idx + 1] == np.inf:
+                self.other_u[idx + 1] = self.other_u[idx]
+            del t
+            return self.other_u[idx + 1]
+        elif self.interpolation_order == 1:
+            previous_other_u = self.other_u[idx]
+            if self.other_u[idx + 1] == np.inf:
+                self.other_u[idx + 1] = previous_other_u
+            next_other_u = self.other_u[idx + 1]
+            # print(f"Interpolating between {previous_other_u}, {next_other_u} at t = {t}")
+            percentage = (t - t_lower) / self.dt
+            # print(f"Percentage: {percentage}")
+            interpolated_u = interpolate_linear(previous_other_u, next_other_u, percentage)
+            # print(f"interpolated value: {interpolated_u}")
+            return interpolated_u
+        else:
+            raise NotImplementedError
+    
+    def second_order_force(self, t, t_lower):
+        if t == 0:
+            return self.k12 * self._initial_other_u()
+        else:
+            return self.k12 * self.other_u_at(t, t_lower)
+
+    def first_order_force(self, t, t_lower):
+        if t == 0:
+            return np.array([0., self.k12 * self._initial_other_u()], dtype=object)
+        else:
+            return np.array([0., self.k12 * self.other_u_at(t, t_lower)], dtype=object)
+
+    def _initial_conditions(self):
+        if self.left_system_bool:
+            u0 = np.array([1.])
+        else:
+            u0 = np.array([0.])
+        v0 = np.array([0.])
+        a0 = np.dot(self.A_second_order, u0) + self.k12 * self._initial_other_u()
+        return np.concatenate([u0, v0, a0])
+    
+    def _initial_other_u(self):
+        if not self.left_system_bool:
+            return 1.
+        else:
+            return 0.
+
+
+class SameTimescales(SystemPartition):
+    def __init__(self,
+                left_system: bool,
+                t_end: float = 0.,
+                N: int = 0,
+                result_values: int = 0):
+        super().__init__(left_system, t_end, N, result_values, 1, 1, 1, 1, 1)
+
+class DiffTimeScales(SystemPartition):
+    def __init__(self,
+                left_system: bool,
+                t_end: float = 0.,
+                N: int = 0,
+                result_values: int = 0):
+        super().__init__(left_system, t_end, N, result_values, 20, 0.1, 0.5, 1, 1)
---
+++ same_timescales/utility.py
@@ -0,0 +1,106 @@
+"""
+Created on Fri Apr 23 16:14:08 2021
+
+@author: valentina
+"""
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def prepare_plot(title, subtitle, xlabel, ylabel):
+    """set up a figure and axis with some basic properties"""
+    plt.rcParams.update({
+        "text.usetex": True,
+        "font.family": "serif",
+        "font.serif": ["Palatino"],
+    })
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+
+    if subtitle != "":
+        fig.suptitle(title, y=1.01, fontsize=14) # make title larger than default
+        ax.set_title(subtitle)
+    else:
+        fig.suptitle(title, fontsize=14) # make title larger than default
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    return fig, ax
+
+def plot_error_ref(ax, dt):
+    """create an error log-log plot in the current axis"""
+    o1 = dt
+    o2 = dt*dt
+    o3 = dt**3
+    o4 = dt**4
+    ax.loglog(dt, o1, 'k-', label='$\mathcal{O}(\Delta t)$', lw=1)
+    ax.loglog(dt, o2, 'k--', label='$\mathcal{O}(\Delta t^2)$', lw=1)
+    ax.loglog(dt, o3, 'k:', label='$\mathcal{O}(\Delta t^3)$', lw=1)
+    ax.loglog(dt, o4, 'k-.', label='$\mathcal{O}(\Delta t^4)$', lw=1)
+    return ax
+
+def plot_displacements(t, sol, path="."):
+    _, ax = prepare_plot("Displacements", "", "t [s]", "u [m]")
+    ax.plot(t, sol[0,:], label="$u_1$")
+    ax.plot(t, sol[1,:], label="$u_2$")
+    ax.legend()
+    plt.savefig(f"{path}/displacements.png", dpi=300, bbox_inches="tight")
+    plt.close()
+
+def plot_velocities(t, sol, path="."):
+    _, ax = prepare_plot("Velocities", "", "t [s]", "v [m/s]")
+    ax.plot(t, sol[2,:], label="$v_1$")
+    ax.plot(t, sol[3,:], label="$v_2$")
+    ax.legend()
+    plt.savefig(f"{path}/velocities.png", dpi=300, bbox_inches="tight")
+    plt.close()
+
+def plot_energy(t, energy, path="."):
+    _, ax = prepare_plot("Energy", "", "t [s]", "Energy")
+    ax.plot(t, energy, label='energy')
+    ax.legend()
+    plt.savefig(f"{path}/energy.png", dpi=300, bbox_inches="tight")
+    plt.close()
+
+def create_solution_plots(t, sol, dir_name: str ="plots"):
+    plotdir_path = f"./{dir_name}"
+    try:
+        os.mkdir(plotdir_path)
+    except FileExistsError:
+        pass
+    energy = compute_energy(sol[0,:], sol[1,:], sol[2,:], sol[3,:])
+    plot_displacements(t, sol, plotdir_path)
+    plot_velocities(t, sol, plotdir_path)
+    plot_energy(t, energy, plotdir_path)
+
+def compute_energy(u1, u2, v1, v2):
+    return v1**2 + v2**2 + u1**2 + u2**2 + (u2-u1)**2
+
+def l1_norm(vec):
+    return np.sum(np.abs(vec))
+
+def l2_norm(vec):
+    return np.sum(vec**2)
+
+def max_norm(vec):
+    return np.max(np.abs(vec))
+
+def analytical_solution(t_end: float, N: int):
+    t = np.linspace(0, t_end, N+1)
+    result = np.array([
+        0.5 * (np.cos(t) + np.cos(np.sqrt(3)*t)),
+        0.5 * (np.cos(t) - np.cos(np.sqrt(3)*t)),
+        0.5 * (- np.sin(t) - np.sqrt(3) * np.sin(np.sqrt(3)*t)),
+        0.5 * (- np.sin(t) + np.sqrt(3) * np.sin(np.sqrt(3)*t)),
+    ])
+    return result
+
+def interpolate_linear(left_value, right_value, percentage):
+    if percentage == 0:
+        return left_value
+    elif percentage == 1:
+        return right_value
+    else:
+        return percentage * right_value + (1 - percentage) * left_value
