fb533fcc459e746fa8783b30b8e5ce3f9e6e9a65
Valentina Schueller <valentina.schueller@gmail.com>
2022-01-07 17:22:39 +0100

    Working Version of Strang Splitting

--- diff_timescales/partitioned_strang.py
+++ diff_timescales/partitioned_strang.py
@@ -33,12 +33,12 @@ def beautify_plot(ax):
 
 if __name__ == '__main__':
     t_stop = 20
-    N_list = np.array([125, 250, 500, 1000, 2000, 4000, 8000])
+    N_list = np.array([1000, 2000, 4000, 8000, 16000])
     dt_list = np.array([t_stop / N for N in N_list])
     errors_newmark_strang = np.array([max_norm(compute_newmark_error(t_stop, N, "strang")) for N in N_list])
     errors_alpha_strang = np.array([max_norm(compute_alpha_error(t_stop, N, "strang")) for N in N_list])
-    errors_erk4_strang = np.array([max_norm(compute_erk4_error(t_stop, N, "strang")) for N in N_list])
     errors_erk1_strang = np.array([max_norm(compute_erk1_error(t_stop, N, "strang")) for N in N_list])
+    errors_erk4_strang = np.array([max_norm(compute_erk4_error(t_stop, N, "strang")) for N in N_list])
 
     title = ""
     subtitle = "Strang Splitting"
@@ -46,11 +46,11 @@ if __name__ == '__main__':
     ylabel = r"$\left\| e \right\|_\infty$"
     fig, ax = prepare_plot(title, subtitle, xlabel, ylabel)
     plot_error_ref(ax, dt_list)
-    ax.plot(dt_list, errors_newmark_strang, linestyle="none", marker="3", color="maroon", label=r"Newmark")
+    ax.plot(dt_list, errors_newmark_strang, linestyle="none", marker="3", color="maroon", label=r"Newmark-$\beta$")
     ax.plot(dt_list, errors_alpha_strang, linestyle="none", marker=".", color="darkcyan", label=r"generalized-$\alpha$")
     ax.plot(dt_list, errors_erk1_strang, linestyle="none", marker="+", color="green", label=r"ERK1")
     ax.plot(dt_list, errors_erk4_strang, linestyle="none", marker="x", color="darkorchid", label=r"ERK4")
 
     ax.legend(ncol=2, loc='lower right')
     ax = beautify_plot(ax)
-    plt.savefig("convergence_same_timescales_partitioned_strang.pdf", dpi=300, bbox_inches='tight')
+    plt.savefig("convergence_diff_timescales_partitioned_strang.pdf", dpi=300, bbox_inches='tight')
