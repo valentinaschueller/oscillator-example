b4c96d8d1ba0ae9e862743be8a9c128d28700681
Valentina Schueller <valentina.schueller@gmail.com>
2022-04-02 15:56:01 +0200

    refactor: docstrings, variable names, opt. params
    - use partition1/2, not left/right (more generic, "side" not relevant)
    - explain code a bit more
    - add docstring to each function
    - cleaner access to values in kwargs
    - tol, max_iters are now optional params for implicit CPS scheme
    - assert that subcycling factor has a sensible value

--- numerical_studies/coupling_schemes.py
+++ numerical_studies/coupling_schemes.py
@@ -1,150 +1,270 @@
 import numpy as np
-
+from system_partition import SystemPartition
+from timestepping import TimesteppingMethod
 from utility import l2_norm
 
 
 def run_cps_simulation(
-    left_system, left_solver, right_system, right_solver, t_end, N, **kwargs
-):
-    if "sc" in kwargs:
-        sc = kwargs["sc"]
-    else:
-        sc = 1
+    partition_1: SystemPartition,
+    solver_1: TimesteppingMethod,
+    partition_2: SystemPartition,
+    solver_2: TimesteppingMethod,
+    t_end: float,
+    N: int,
+    **kwargs,
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    run a partitioned simulation with the conventional parallel staggered (CPS) scheme
+
+    both partitions are updated using the interface values from the previous time step.
+    In theory, the computations could thus happen in parallel (although they are sequential here).
+
+    supports subcycling using the optional parameter 'sc', denoting the subcycling factor.
+    If sc > 1: partition_1 does sc*N time steps (partition_2 does N time steps).
+
+    :param partition_1: the first partition
+    :param solver_1: the timestepping method associated with partition_1
+    :param partition_2: the second system partition
+    :param solver_2: the timestepping method associated with partition_2
+    :param t_end: the simulation is executed until t >= t_end
+    :param N: the number of time steps
+    :returns: a tuple containing the result of partition_1 and partition_2 (N values)
+    :raises ValueError: if subcycling factor is invalid
+    """
+    sc = kwargs.get("sc", 1)
+    if type(sc) is not int or sc < 1:
+        raise ValueError(f"subcycling factor must be a positive integer")
+
     dt = t_end / N
     dt_sc = dt / sc
     t = 0
-    right_iter = 0
-    left_iter = 0
-    while right_iter < N:
+
+    iter_1 = 0
+    iter_2 = 0
+    while iter_2 < N:
+        # partition_1 does sc time steps per time window
         for i in range(sc):
-            left_system.result[:, left_iter + 1] = np.squeeze(
-                left_solver.compute_timestep(
-                    dt_sc, t + i * dt_sc, left_system.result[:, left_iter]
+            partition_1.result[:, iter_1 + 1] = np.squeeze(
+                solver_1.compute_timestep(
+                    dt_sc, t + i * dt_sc, partition_1.result[:, iter_1]
                 )
             )
-            left_iter += 1
-        right_system.result[:, right_iter + 1] = np.squeeze(
-            right_solver.compute_timestep(dt, t, right_system.result[:, right_iter])
+            iter_1 += 1
+        # partition_2 does 1 time step per time window
+        partition_2.result[:, iter_2 + 1] = np.squeeze(
+            solver_2.compute_timestep(dt, t, partition_2.result[:, iter_2])
         )
-        right_iter += 1
-        t += dt
-        left_system.other_u[right_iter] = right_system.result[0, right_iter]
-        right_system.other_u[left_iter - (sc - 1) : left_iter + 1] = left_system.result[
-            0, left_iter - (sc - 1) : left_iter + 1
+        iter_2 += 1
+        # update the interface values handed to the partitions *after* both computations are done
+        # note: partition_1 provides 'sc' interface values in each time step (as opposed to one)
+        partition_1.other_u[iter_2] = partition_2.result[0, iter_2]
+        partition_2.other_u[iter_1 - (sc - 1) : iter_1 + 1] = partition_1.result[
+            0, iter_1 - (sc - 1) : iter_1 + 1
         ]
-    return left_system.result[:, ::sc], right_system.result
+        t += dt
+    return partition_1.result[:, ::sc], partition_2.result
 
 
 def run_css_simulation(
-    left_system, left_solver, right_system, right_solver, t_end, N, **kwargs
-):
-    """left system does the first step"""
-    if "sc" in kwargs:
-        sc = kwargs["sc"]
-    else:
-        sc = 1
+    partition_1: SystemPartition,
+    solver_1: TimesteppingMethod,
+    partition_2: SystemPartition,
+    solver_2: TimesteppingMethod,
+    t_end: float,
+    N: int,
+    **kwargs,
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    run a partitioned simulation with the conventional serial staggered (CSS) scheme
+
+    partition_1 does the first step, partition_2 uses the newly computed interface value(s).
+
+    supports subcycling using the optional parameter 'sc', denoting the subcycling factor.
+    If sc > 1: partition_1 does sc*N time steps (partition_2 does N time steps).
+
+    :param partition_1: the first partition
+    :param solver_1: the timestepping method associated with partition_1
+    :param partition_2: the second system partition
+    :param solver_2: the timestepping method associated with partition_2
+    :param t_end: the simulation is executed until t >= t_end
+    :param N: the number of time steps
+    :returns: a tuple containing the result of partition_1 and partition_2 (N values)
+    :raises ValueError: if subcycling factor is invalid
+    """
+    sc = kwargs.get("sc", 1)
+    if type(sc) is not int or sc < 1:
+        raise ValueError(f"subcycling factor must be a positive integer")
+
     dt = t_end / N
     dt_subcycling = dt / sc
     t = 0
-    right_iter = 0
-    left_iter = 0
-    while right_iter < N:
+
+    iter_1 = 0
+    iter_2 = 0
+    while iter_2 < N:
+        # partition_1 does sc time steps per time window
         for i in range(sc):
-            left_system.result[:, left_iter + 1] = np.squeeze(
-                left_solver.compute_timestep(
+            partition_1.result[:, iter_1 + 1] = np.squeeze(
+                solver_1.compute_timestep(
                     dt_subcycling,
                     t + i * dt_subcycling,
-                    left_system.result[:, left_iter],
+                    partition_1.result[:, iter_1],
                 )
             )
-            left_iter += 1
-        right_system.other_u[left_iter - (sc - 1) : left_iter + 1] = left_system.result[
-            0, left_iter - (sc - 1) : left_iter + 1
+            iter_1 += 1
+        # update interface values for partition_2 (sc values)
+        partition_2.other_u[iter_1 - (sc - 1) : iter_1 + 1] = partition_1.result[
+            0, iter_1 - (sc - 1) : iter_1 + 1
         ]
-        right_system.result[:, right_iter + 1] = np.squeeze(
-            right_solver.compute_timestep(dt, t, right_system.result[:, right_iter])
+        # partition_2 does 1 time step per time window
+        partition_2.result[:, iter_2 + 1] = np.squeeze(
+            solver_2.compute_timestep(dt, t, partition_2.result[:, iter_2])
         )
-        right_iter += 1
+        iter_2 += 1
+        # update interface value for partition_1 (1 value)
+        partition_1.other_u[iter_2] = partition_2.result[0, iter_2]
         t += dt
-        left_system.other_u[right_iter] = right_system.result[0, right_iter]
-    return left_system.result[:, ::sc], right_system.result
+    return partition_1.result[:, ::sc], partition_2.result
 
 
 def run_implicit_cps_simulation(
-    left_system, left_solver, right_system, right_solver, t_end, N, **kwargs
+    partition_1: SystemPartition,
+    solver_1: TimesteppingMethod,
+    partition_2: SystemPartition,
+    solver_2: TimesteppingMethod,
+    t_end: float,
+    N: int,
+    **kwargs,
 ):
-    if "sc" in kwargs:
-        sc = kwargs["sc"]
-    else:
-        sc = 1
-    tol = 1e-8
-    max_iters = 10
+    """
+    run a partitioned simulation with the implicit conventional parallel staggered (CPS) scheme
+
+    Implicit --> fixed-point iterations are used until the interface values converge between iterations.
+    both partitions are updated using the interface values from the previous time step.
+    In theory, the computations could thus happen in parallel (although they are sequential here).
+
+    supports subcycling using the optional parameter 'sc', denoting the subcycling factor.
+    If sc > 1: partition_1 does sc*N time steps (partition_2 does N time steps).
+
+    Set convergence tolerance using optional param 'tol'
+    Set max. amount of fixed-point iterations using optional param 'max_iters'
+
+    :param partition_1: the first partition
+    :param solver_1: the timestepping method associated with partition_1
+    :param partition_2: the second system partition
+    :param solver_2: the timestepping method associated with partition_2
+    :param t_end: the simulation is executed until t >= t_end
+    :param N: the number of time steps
+    :returns: a tuple containing the result of partition_1 and partition_2 (N values)
+    :raises ValueError: if subcycling factor is invalid
+    """
+    sc = kwargs.get("sc", 1)
+    if type(sc) is not int or sc < 1:
+        raise ValueError(f"subcycling factor must be a positive integer")
+
+    tol = kwargs.get("tol", 1e-8)
+    max_iters = kwargs.get("max_iters", 10)
+
     dt = t_end / N
     dt_sc = dt / sc
     t = 0
-    right_iter = 0
-    left_iter = 0
-    while right_iter < N:
-        fp_iter = 0
+
+    iter_1 = 0
+    iter_2 = 0
+    while iter_2 < N:
+        fixed_point_iter = 0
         res = np.inf
-        while (fp_iter < max_iters) and (res > tol):
-            left_temp = np.full((left_system.result.shape[0], sc + 1), np.inf)
-            left_temp[:, 0] = left_system.result[:, left_iter]
+        while (fixed_point_iter < max_iters) and (res > tol):
+            # compute temporary new values of partition_1
+            # partition_1 does sc steps per time window
+            temp_1 = np.full((partition_1.result.shape[0], sc + 1), np.inf)
+            temp_1[:, 0] = partition_1.result[:, iter_1]
             for i in range(sc):
-                left_temp[:, i + 1] = np.squeeze(
-                    left_solver.compute_timestep(dt_sc, t + i * dt_sc, left_temp[:, i])
+                temp_1[:, i + 1] = np.squeeze(
+                    solver_1.compute_timestep(dt_sc, t + i * dt_sc, temp_1[:, i])
                 )
-            right_temp = np.squeeze(
-                right_solver.compute_timestep(dt, t, right_system.result[:, right_iter])
+            # compute temporary new value of partition_2
+            # partition_1 does 1 step per time window
+            temp_2 = np.squeeze(
+                solver_2.compute_timestep(dt, t, partition_2.result[:, iter_2])
             )
-            res = l2_norm(
-                left_temp[:, -1] - left_system.result[:, left_iter + sc]
-            ) + l2_norm(right_temp - right_system.result[:, right_iter + 1])
-            assert (
-                np.sum(np.abs(left_system.result[:, left_iter] - left_temp[:, 0])) == 0
-            )
-            left_system.result[:, left_iter : (left_iter + 1 + sc)] = left_temp.copy()
-            right_system.result[:, right_iter + 1] = right_temp
-            left_system.other_u[right_iter + 1] = right_temp[0]
-            right_system.other_u[left_iter : (left_iter + 1 + sc)] = left_temp[0].copy()
-            fp_iter += 1
-        right_iter += 1
-        left_iter += sc
+            # compute residual: l2 norm of result difference between previous and current iteration
+            # evaluated at the end of the time window
+            res_1 = l2_norm(temp_1[:, -1] - partition_1.result[:, iter_1 + sc])
+            res_2 = l2_norm(temp_2 - partition_2.result[:, iter_2 + 1])
+            res = res_1 + res_2
+            # copy temporary values to result arrays
+            partition_1.result[:, iter_1 : (iter_1 + 1 + sc)] = temp_1.copy()
+            partition_2.result[:, iter_2 + 1] = temp_2
+            # update interface values of partitions
+            partition_1.other_u[iter_2 + 1] = temp_2[0]
+            partition_2.other_u[iter_1 : (iter_1 + 1 + sc)] = temp_1[0].copy()
+            fixed_point_iter += 1
+        iter_1 += sc
+        iter_2 += 1
         t += dt
-    assert left_iter == sc * N
-    assert right_iter == N
-    assert np.sum(np.abs(left_system.result[0] - right_system.other_u)) == 0
-    assert np.sum(np.abs(right_system.result[0] - left_system.other_u)) == 0
+    # some assertions to make sure we didn't do anything wrong
+    assert iter_1 == sc * N
+    assert iter_2 == N
+    assert np.sum(np.abs(partition_1.result[0] - partition_2.other_u)) == 0
+    assert np.sum(np.abs(partition_2.result[0] - partition_1.other_u)) == 0
     assert np.abs(t - t_end) < 1e-10
-    return left_system.result[:, ::sc], right_system.result
+    return partition_1.result[:, ::sc], partition_2.result
 
 
 def run_strang_simulation(
-    left_system, left_solver, right_system, right_solver, t_end, N, **kwargs
+    partition_1: SystemPartition,
+    solver_1: TimesteppingMethod,
+    partition_2: SystemPartition,
+    solver_2: TimesteppingMethod,
+    t_end: float,
+    N: int,
+    **kwargs,
 ):
-    """left system = f1, right system = f2"""
+    """
+    run a partitioned simulation with Strang splitting
+
+    1. partition_1 does a half dt/2 step
+    2. partition_2 does a full dt step
+    3. partition_1 does a half dt/2 step
+
+    does not support subcycling!
+
+    :param partition_1: the first partition
+    :param solver_1: the timestepping method associated with partition_1
+    :param partition_2: the second system partition
+    :param solver_2: the timestepping method associated with partition_2
+    :param t_end: the simulation is executed until t >= t_end
+    :param N: the number of time steps
+    :returns: a tuple containing the result of partition_1 and partition_2 (N values)
+    """
     del kwargs
+
     dt = t_end / N
     t = 0
-    right_iter = 0
-    left_iter = 0
-    while right_iter < N:
-        left_system.result[:, left_iter + 1] = np.squeeze(
-            left_solver.compute_timestep(dt / 2, t, left_system.result[:, left_iter])
+
+    iter_1 = 0
+    iter_2 = 0
+    while iter_2 < N:
+        # half step
+        partition_1.result[:, iter_1 + 1] = np.squeeze(
+            solver_1.compute_timestep(dt / 2, t, partition_1.result[:, iter_1])
         )
-        left_iter += 1
-        right_system.other_u[right_iter + 1] = left_system.result[0, left_iter]
-        right_system.result[:, right_iter + 1] = np.squeeze(
-            right_solver.compute_timestep(dt, t, right_system.result[:, right_iter])
+        iter_1 += 1
+        # update interface value for full step
+        partition_2.other_u[iter_2 + 1] = partition_1.result[0, iter_1]
+        # full step
+        partition_2.result[:, iter_2 + 1] = np.squeeze(
+            solver_2.compute_timestep(dt, t, partition_2.result[:, iter_2])
         )
-        right_iter += 1
-        left_system.other_u[left_iter] = right_system.result[0, right_iter]
-        left_system.other_u[left_iter + 1] = right_system.result[0, right_iter]
-        left_system.result[:, left_iter + 1] = np.squeeze(
-            left_solver.compute_timestep(
-                dt / 2, t + dt / 2, left_system.result[:, left_iter]
-            )
+        iter_2 += 1
+        # update interface value for this step *and* the next step
+        partition_1.other_u[iter_1] = partition_2.result[0, iter_2]
+        partition_1.other_u[iter_1 + 1] = partition_2.result[0, iter_2]
+        # half step
+        partition_1.result[:, iter_1 + 1] = np.squeeze(
+            solver_1.compute_timestep(dt / 2, t + dt / 2, partition_1.result[:, iter_1])
         )
-        left_iter += 1
+        iter_1 += 1
         t += dt
-    return left_system.result[:, ::2], right_system.result
+    return partition_1.result[:, ::2], partition_2.result
